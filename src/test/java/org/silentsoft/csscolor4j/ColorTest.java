cee07fae8fb2d539b12904db4b65030cf2f537cf
slisaasquatch <sli@saasquat.ch>
2025-05-17 09:44:41 -0700

    Test for rgba hex

--- src/test/java/org/silentsoft/csscolor4j/ColorTest.java
+++ src/test/java/org/silentsoft/csscolor4j/ColorTest.java
@@ -121,6 +121,8 @@ public class ColorTest {
     @Test
     public void hexTest() {
         Assertions.assertEquals("#ffffff", Color.valueOf("rgb(255, 255, 255)").getHex());
+        Assertions.assertEquals("#ffffff", Color.valueOf("rgba(255, 255, 255, 1.0)").getHex());
+        Assertions.assertEquals("#ffffff80", Color.valueOf("rgba(255, 255, 255, 0.5)").getHex());
 
         Assertions.assertEquals("#9cf", Color.valueOf("9cf").getHex());
         Assertions.assertEquals("#9cf", Color.valueOf("#9cf").getHex());
